075f61db54e7bca991862e207950aaeccbbbab6e
Mann Malviya <mannmalviya15@gmail.com>
2026-01-03 00:18:53 -0800

    trying trapping rain water

---
+++ python/Medium/78_Subsets.py
@@ -0,0 +1,37 @@
+"""
+LeetCode Problem: 78. Subsets
+Link:
+- https://leetcode.com/problems/subsets/description/
+
+Date Solved: Jan 2nd, 2026
+Total Time Spent: 
+
+Thought Process: 
+- I've definetly done this one, maybe in a different context or in cse102 before.
+- This problem
+
+Notes:
+- NeetCode 150, 1st problem under Backtracking
+"""
+
+from typing import List
+
+class Solution():
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        """
+        
+            Time Complexity: O()
+            Space Complexity: O()
+        """
+
+        return
+    
+def main():
+    ans_obj = Solution()
+
+    print(ans_obj.subsets([1]))
+
+    return
+
+if __name__ == "__main__":
+    main()
